657fef2884bbc03cd34ef9657ff209186e5c650b
msclark <msclark3@asu.edu>
2018-12-09 15:05:10 -0700

    installed prisma and initialized

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cb7d45a15d529868317c";
+/******/ 	var hotCurrentHash = "9f3694fb32b1e68f62a5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
