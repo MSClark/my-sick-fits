ecdde107130b28a755df687d14f7388e1a9611e2
msclark <msclark3@asu.edu>
2018-12-09 12:52:46 -0700

    added NProgress to header

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b10110e53a5616a2c31a";
+/******/ 	var hotCurrentHash = "cb7d45a15d529868317c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
