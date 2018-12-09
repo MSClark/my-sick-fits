cdf1a6daadbcfa3d4897e6179220a4776007e835
msclark <msclark3@asu.edu>
2018-12-09 16:26:22 -0700

    added server start to index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,20 @@
 // let's go!
+require('dotenv').config({path: 'variables.env'});
+const createServer = require('createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO use express middleqare to handle cookies (JWT) and populate current user
+server.start(
+    {
+        cors: {
+            credentials: true, 
+            origin: process.env.FRONTEND_URL,
+        },
+
+    }, 
+    deets => {
+        console.log(`server is now running on port http://localhost:${deets.port}`);
+    }
+);
